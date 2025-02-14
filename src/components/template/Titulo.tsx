fe9b8c881ae803cac85217e87980e458e9777b3e
Edson Garcia <ednosmab@yahoo.com.br>
2025-02-14 17:48:44 -0300

    add style in the title component

--- src/components/template/Titulo.tsx
+++ src/components/template/Titulo.tsx
@@ -6,10 +6,10 @@ interface TituloProps {
 export default function Titulo (props: TituloProps){
     return (
         <div>
-            <h1>
+            <h1 className={`font-black text-3xl text-gray-900`}>
                     {props.titulo}
             </h1>
-            <h2>
+            <h2 className={`font-light text-sm text-gray-600`}>
                     {props.subTitulo}
             </h2>
         </div>
